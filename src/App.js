b9846f49ed1eb96bb77ecbf3f4137305fe9358c9
srishtimishra123 <srishtisrija@gmail.com>
2022-11-30 11:58:56 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import New from './component/New'
+import New2 from './component/New2'
 
 const App = () => {
   return (
    <>
    <p>app</p>
    <New/>
+   <New2/>
    </>
   )
 }
---
+++ src/component/New2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const New2 = () => {
+  return (
+    <div>New2</div>
+  )
+}
+
+export default New2
